c96d777dabc30080e3433267e1c70436b69bfd76
Shobham Rajak <shobham.rajak@gmail.com>
2023-01-24 16:41:23 +0530

    add model and controller for comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,7 +32,12 @@ class PostsController < ApplicationController
 
     def edit
         @post = Post.find(params[:id])
-        
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.delete
+        redirect_to posts_path
     end
 
     private
