f6124a798d8aa614791bb731ae76faf1ce5c408a
Shobham Rajak <shobham.rajak@gmail.com>
2023-01-27 13:44:43 +0530

    add migration and routes for comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :posts
-  root "posts#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :posts do 
+    resources :comments
+  end
+  root "posts#index"
 end
